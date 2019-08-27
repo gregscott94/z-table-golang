8ecba34eaa1d2584b59c76820cf05726a69f53c7
Greg Scott <gregscott94@gmail.com>
2019-08-27 10:11:02 -0400

    Adding integration to get true z-table percentage values and logic to build a tree for indexing the percentages for faster lookup

--- ztable.go
+++ ztable.go
@@ -1,13 +1,17 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"math"
+	"runtime"
+
+	"gonum.org/v1/gonum/integrate/quad"
 )
 
 type ZTable struct {
-	zScoreMap   map[float64]int
+	zScoreMap   map[string]int
 	percentages []float64
+	rootNode    *Node
 }
 
 type ZTableOptions struct {
@@ -27,13 +31,14 @@ func NewZTable(options *ZTableOptions) *ZTable {
 		bucketSize = options.BucketSize
 	}
 	zTable := ZTable{
-		zScoreMap:   make(map[float64]int),
+		zScoreMap:   make(map[string]int),
 		percentages: []float64{},
 	}
 	zScore := float64(-4)
 	leafNodes := []*Node{}
 	for zScore <= 4 {
-		percentage := (1 / math.Sqrt(2*math.Pi)) * math.Exp((-1*zScore*zScore)/2)
+		concurrent := runtime.GOMAXPROCS(0)
+		percentage := quad.Fixed(normalProbabilityDensity, math.Inf(-1), zScore, 1000, nil, concurrent)
 		index := len(zTable.percentages)
 		if index%bucketSize == 0 {
 			leafNodes = append(leafNodes, &Node{
@@ -41,13 +46,39 @@ func NewZTable(options *ZTableOptions) *ZTable {
 				index: index,
 			})
 		}
-		zTable.zScoreMap[zScore] = index
+		zTable.zScoreMap[fmt.Sprintf(`%.2f`, zScore)] = index
 		zTable.percentages = append(zTable.percentages, percentage)
 		zScore = zScore + 0.01
+	}
+	rootNodeSlice := buildTree(leafNodes)
+	if len(rootNodeSlice) > 0 {
+		zTable.rootNode = rootNodeSlice[0]
 	}
 	return &zTable
 }
 
+func normalProbabilityDensity(x float64) float64 {
+	return (1 / math.Sqrt(2*math.Pi)) * math.Exp((-1*x*x)/2)
+}
+
+func buildTree(layer []*Node) []*Node {
+	currLayer := []*Node{}
+	count := 0
+	for count < len(layer)-1 {
+		currLayer = append(currLayer, &Node{
+			value:     (layer[count].value + layer[count+1].value) / 2,
+			leftNode:  layer[count],
+			rightNode: layer[count+1],
+		})
+		count = count + 1
+	}
+	if len(currLayer) <= 1 {
+		return currLayer
+	}
+	return buildTree(currLayer)
+}
+
 func main() {
-	log.Println("Init commit")
+	zt := NewZTable(nil)
+	fmt.Println(zt)
 }
